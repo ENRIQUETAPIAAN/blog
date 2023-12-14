3a54a08d8f035ae6450074a561652915d9bc2c00
enriquetapiaan <tapiaa.antunes@outlook.com>
2023-12-14 13:36:55 -0600

    obteniendo los articulos de la base de datos

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function conexion($db_config){
     try {
-        $conexion = new PDO('mysql:host=localhost;dbname=db_blog','root','');
+        $conexion = new PDO('mysql:host=localhost;dbname='.$db_config['basedatos'], $db_config['usuario'], $db_config['pass']);
         return $conexion;
     } catch (PDOException $e) {
         return false;
@@ -14,6 +14,17 @@ function limpiarDatos($datos){
     $datos = stripcslashes($datos);
     $datos = htmlspecialchars($datos);
     return $datos;
+}
+
+function pagina_actual(){
+    return isset($_GET['p']) ? (int)$_GET['p'] : 1;
+}
+
+function obtener_post($post_por_pagina, $conexion){
+    $inicio = (pagina_actual() > 1) ? pagina_actual() * $post_por_pagina - $post_por_pagina : 0;
+    $sentencia = $conexion->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM tb_articulos LIMIT $inicio, $post_por_pagina");
+    $sentencia->execute();
+    return $sentencia->fetchAll();
 }
 
 ?>
