76dc29b819dd6d52bd9ba2db631e924aa97c690c
enriquetapiaan <tapiaa.antunes@outlook.com>
2023-12-14 11:32:00 -0600

    acabando la pagina de inicio

---
+++ views/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <p class="copyright">Copyright | Enrique Tapia | 2023</p>
+    </footer>
+</body>
+</html>
--- views/index.view.php
+++ views/index.view.php
@@ -1,39 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;700&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/07702d4e25.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="<?php echo RUTA; ?>css/estiloss.css">
-    <title>Blog | Inicio</title>
-</head>
-<body>
-    <header>
-        <div class="contenedor">
-            <div class="logo izquierda">
-                <p><a href="#">Mi Primer Blog</a></p>
-            </div>
-            <div class="derecha">
-                <form name="busqueda" class="buscar" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="get">
-                    <input type="text" name="busqueda" placeholder="Buscar:">
-                    <button type="submit" class="icono fa fa-search"></button>
-                </form>
-                <nav class="menu">
-                    <ul>
-                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                        <li><a href="#">Contacto  <i class="fa fa-envelope"></i></a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </header>
+<?php require 'header.php';?>
 
 
     <div class="contenedor">
@@ -53,34 +18,15 @@
                 <h2 class="titulo">Titulo del articulo</h2>
                 <p class="fecha">miércoles, 14 de diciembre del 2023</p>
                 <div class="thumb">
-                    <a href="#"><img src="<?php echo RUTA; ?>/img/1.png"></a>
+                    <a href="#"><img src="<?php echo RUTA; ?>/img/2.png"></a>
                 </div>
                 <p class="extracto">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, distinctio.</p>
                 <a href="#" class="continuar">Continuar Leyendo...</a>
             </article>
         </div>
-        <div class="post">
-            <article>
-                <h2 class="titulo">Titulo del articulo</h2>
-                <p class="fecha">miércoles, 14 de diciembre del 2023</p>
-                <div class="thumb">
-                    <a href="#"><img src="<?php echo RUTA; ?>/img/1.png"></a>
-                </div>
-                <p class="extracto">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, distinctio.</p>
-                <a href="#" class="continuar">Continuar Leyendo...</a>
-            </article>
-        </div>
-        <div class="post">
-            <article>
-                <h2 class="titulo">Titulo del articulo</h2>
-                <p class="fecha">miércoles, 14 de diciembre del 2023</p>
-                <div class="thumb">
-                    <a href="#"><img src="<?php echo RUTA; ?>/img/1.png"></a>
-                </div>
-                <p class="extracto">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, distinctio.</p>
-                <a href="#" class="continuar">Continuar Leyendo...</a>
-            </article>
-        </div>
+
+        <?php require 'paginacion.php'; ?>
+
     </div>
-</body>
-</html>
+
+    <?php require 'footer.php'; ?>
