79d711015a606185b15ca62261ddeb2c656a5c56
enriquetapiaan <tapiaa.antunes@outlook.com>
2023-12-14 11:16:21 -0600

    agregue más vistas del inicio

--- views/index.view.php
+++ views/index.view.php
@@ -34,5 +34,53 @@
             </div>
         </div>
     </header>
+
+
+    <div class="contenedor">
+        <div class="post">
+            <article>
+                <h2 class="titulo">Titulo del articulo</h2>
+                <p class="fecha">miércoles, 14 de diciembre del 2023</p>
+                <div class="thumb">
+                    <a href="#"><img src="<?php echo RUTA; ?>/img/1.png"></a>
+                </div>
+                <p class="extracto">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, distinctio.</p>
+                <a href="#" class="continuar">Continuar Leyendo...</a>
+            </article>
+        </div>
+        <div class="post">
+            <article>
+                <h2 class="titulo">Titulo del articulo</h2>
+                <p class="fecha">miércoles, 14 de diciembre del 2023</p>
+                <div class="thumb">
+                    <a href="#"><img src="<?php echo RUTA; ?>/img/1.png"></a>
+                </div>
+                <p class="extracto">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, distinctio.</p>
+                <a href="#" class="continuar">Continuar Leyendo...</a>
+            </article>
+        </div>
+        <div class="post">
+            <article>
+                <h2 class="titulo">Titulo del articulo</h2>
+                <p class="fecha">miércoles, 14 de diciembre del 2023</p>
+                <div class="thumb">
+                    <a href="#"><img src="<?php echo RUTA; ?>/img/1.png"></a>
+                </div>
+                <p class="extracto">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, distinctio.</p>
+                <a href="#" class="continuar">Continuar Leyendo...</a>
+            </article>
+        </div>
+        <div class="post">
+            <article>
+                <h2 class="titulo">Titulo del articulo</h2>
+                <p class="fecha">miércoles, 14 de diciembre del 2023</p>
+                <div class="thumb">
+                    <a href="#"><img src="<?php echo RUTA; ?>/img/1.png"></a>
+                </div>
+                <p class="extracto">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, distinctio.</p>
+                <a href="#" class="continuar">Continuar Leyendo...</a>
+            </article>
+        </div>
+    </div>
 </body>
 </html>
