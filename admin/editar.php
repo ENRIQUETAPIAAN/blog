d7bb2f3755b04d13f9dd45919205a7be2fe5b565
enriquetapiaan <tapiaa.antunes@outlook.com>
2023-12-30 16:27:11 -0600

    avanzando a pagina para editar articulos

--- admin/editar.php
+++ admin/editar.php
@@ -12,6 +12,25 @@ if(!$conexion){
 }
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $titulo = limpiarDatos($_POST['titulo']);
+    $extracto = limpiarDatos($_POST['extracto']);
+    $texto = $_POST['texto'];
+    $id = limpiarDatos($_POST['id']);
+    $thumb_gardada = $_POST['thumb-gardada'];
+    $thumb = $_FILES['thumb'];
+
+    if(empty($humb['name'])){
+        $thumb = $thumb_gardada;
+    } else{
+        $archivo_subido = '../' . $blog_config['carpeta_imagenes'] . $_FILES['thumb']['name'];  
+        move_uploaded_file($_FILES['thumb']['tmp_name'], $archivo_subido);
+        $thumb = $_FILES['thumb']['name'];
+    }
+
+    $statement = $conexion->prepare('UPDATE tb_articulos SET titulo_articulo = :titulo, extracto_articulo = :extracto, texto_articulo = :texto, thumb = :thumb WHERE id_articulo = :id');
+    $statement->execute(array(':titulo' => $titulo,':extracto' => $extracto, ':texto' => $texto, ':thumb' => $thumb, ':id' => $id));
+
+    header('Location: ' . RUTA . 'admin');
 
 } else{
     $id_articulo = id_articulo($_GET['id']);
@@ -25,7 +44,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     if(!$post){
         header('Location: ' . RUTA . 'admin');
     }
-    
+
     $post = $post[0];
 }
 
